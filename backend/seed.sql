54f3abc9ea41ee08da494725608af9212bdb4ada
Yves Bächtiger <yvesbachtiger@Yvess-MacBook-Air.local>
2022-08-23 15:52:29 +0200

    counter change

--- backend/seed.sql
+++ backend/seed.sql
@@ -1,9 +1,9 @@
 // Initialize DB 
 
 INSERT INTO ticketing_counter (id, counter_name)
-VALUES (1, 'Schalter A'),
-(2, 'Schalter B')
-(3, 'Schalter C');
+VALUES (1, 'Schalter 1'),
+(2, 'Schalter 2')
+(3, 'Schalter 3');
 
 // The Zero-Ticket is always needed to start the counter
 // Fix in later version
