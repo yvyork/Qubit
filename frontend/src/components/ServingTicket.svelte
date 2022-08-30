c46092cc92f1c66f33fa8ce7fb7f9f18733b83a0
Yves Bächtiger <yvesbachtiger@Yvess-MacBook-Air.local>
2022-08-30 17:45:24 +0200

    url update

--- frontend/src/components/ServingTicket.svelte
+++ frontend/src/components/ServingTicket.svelte
@@ -6,9 +6,9 @@
     
     const callAgain = async (e) => {
         const url_pacs = "http://10.65.15.141:3100/api/rank";
-        const url = "http://10.65.15.159:3100/api/rank";
+        // const url = "http://10.65.15.159:3100/api/rank";
         let ticket = e.detail;
-        const res = await fetch(url, {
+        const res = await fetch(url_pacs, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json'},
             body: JSON.stringify({
